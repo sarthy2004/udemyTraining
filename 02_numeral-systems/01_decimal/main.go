9250c41e4c7deb8ed45bd4cf4252887bdf725589
Parthasarthy Rajarathinam <sarthy2000@yahoo.co.uk>
2018-04-09 02:15:57 -0500

    last commit

--- 02_numeral-systems/01_decimal/main.go
+++ 02_numeral-systems/01_decimal/main.go
@@ -6,7 +6,11 @@ import (
 
 func main() {
 	fmt.Println(42)
-	fmt.Printf("%d - %b", 42, 42)
+	fmt.Printf("%d - %b\n", 42, 42)
 	fmt.Printf("%d - %c\n", 42, 42)
 	fmt.Printf("%d - %c - %x - %X - %#X \n", 42, 42,42, 42,42)
+	
+	for i:=0; i < 200; i++{
+	fmt.Printf("%d - %c - %x - %X - %#X \n", i, i, i,i,i)
+	}
 }
